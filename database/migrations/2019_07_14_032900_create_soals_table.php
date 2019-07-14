7f0adffae9b8103aff5dfbd6ca9d0da50410d766
itlabil <njajal.studio8@gmail.com>
2019-07-14 10:45:36 +0700

    First Upload

---
+++ database/migrations/2019_07_14_032900_create_soals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text("soal");
+            $table->text("jawaban_a");
+            $table->text("jawaban_b");
+            $table->text("jawaban_c");
+            $table->text("jawaban_d");
+            $table->string("knc_jawaban");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soals');
+    }
+}
